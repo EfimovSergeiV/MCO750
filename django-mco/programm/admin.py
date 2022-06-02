236a60e83366aa0344f19f489a6e4e1d545929fa
anon <sys@tehnosvar.ru>
2022-06-02 15:33:28 +0300

    edd: dissabled sort by name to admin

--- django-mco/programm/admin.py
+++ django-mco/programm/admin.py
@@ -21,7 +21,14 @@ from programm.models import (
     CorrectorSectionModel
 )
 
+from django.template.response import TemplateResponse
+from django.http import Http404
+from django.utils.translation import gettext as _
+
+
 class MyAdminSite(AdminSite):
+    """ Отключаем сортировки по названию в админке"""
+
     site_header = 'MCO750'
     site_title = 'MCO750'
     index_title = 'Управление сварочной машиной'
@@ -41,6 +48,30 @@ class MyAdminSite(AdminSite):
         #     app["models"].sort(key=lambda x: x["name"])
 
         return app_list
+
+    def app_index(self, request, app_label, extra_context=None):
+        app_dict = self._build_app_dict(request, app_label)
+        if not app_dict:
+            raise Http404("The requested admin page does not exist.")
+        # Sort the models alphabetically within each app.
+        # app_dict["models"].sort(key=lambda x: x["name"])
+        context = {
+            **self.each_context(request),
+            "title": _("%(app)s administration") % {"app": app_dict["name"]},
+            "subtitle": None,
+            "app_list": [app_dict],
+            "app_label": app_label,
+            **(extra_context or {}),
+        }
+
+        request.current_app = self.name
+
+        return TemplateResponse(
+            request,
+            self.app_index_template
+            or ["admin/%s/app_index.html" % app_label, "admin/app_index.html"],
+            context,
+        )
 
 
 admin.site = MyAdminSite()
