d4664fcfbd251fe05fe0369e14fe3adb1bfcdd2e
anon <sys@tehnosvar.ru>
2022-06-02 17:08:42 +0300

    add: database structure

--- source/backend/database.py
+++ source/backend/database.py
@@ -1,3 +1,41 @@
+"""
+Tables: 
+
+[
+    ('django_migrations',), 
+    ('django_content_type',), 
+    ('auth_permission',), 
+    ('auth_group',), 
+    ('auth_group_permissions',), 
+    ('auth_user',), 
+    ('auth_user_groups',), 
+    ('auth_user_user_permissions',),
+    ('django_admin_log',), 
+    ('django_session',), 
+
+    ('programm_programmmodel',), 
+    ('programm_correctorparammodel',), 
+    ('programm_reflowparammodel',), 
+    ('programm_sedimentpressuresensormodel',), 
+    ('programm_primaryvoltagesensormodel',), 
+    ('programm_preheatingmodel',), 
+    ('programm_positionsensormodel',), 
+    ('programm_pkpressuremetersensormodel',), 
+    ('programm_otherparametersensormodel',), 
+    ('programm_oiltemperaturesensormodel',), 
+    ('programm_nkpressuremetersensormodel',), 
+    ('programm_hydraulicpressuresensormodel',), 
+    ('programm_currentsensormodel',), 
+    ('programm_burningmodel',), 
+    ('programm_clampmodel',), 
+    ('programm_reflowsectionmodel',), 
+    ('programm_correctorsectionmodel',)
+]
+
+"""
+
+
+
 DATABASE = {
     'default': {
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -10,6 +48,8 @@ DATABASE = {
 }
 
 default_db = DATABASE['default']
+
+sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
 
 from email.policy import default
 import psycopg2
@@ -26,19 +66,16 @@ try:
 
     # Курсор для выполнения операций с базой данных
     cursor = connection.cursor()
-    # Распечатать сведения о PostgreSQL
-    print("Информация о сервере PostgreSQL")
-    print(connection.get_dsn_parameters(), "\n")
-    # Выполнение SQL-запроса
-    cursor.execute("SELECT version();")
-    # Получить результат
-    record = cursor.fetchone()
-    print("Вы подключены к - ", record, "\n")
+    cursor.execute(sql)
+    tables = cursor.fetchall()
+    print(tables)
+
 
 except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("Соединение с PostgreSQL закрыто")
+    pass
+#     print("Ошибка при работе с PostgreSQL", error)
+# finally:
+#     if connection:
+#         c.close()
+#         connection.close()
+#         print("Соединение с PostgreSQL закрыто")
